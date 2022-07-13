9749c0c112617b3170b69728589c073935f0f637
munichandra-nageti <107548372+munichandra-nageti@users.noreply.github.com>
2022-07-14 00:44:11 +0530

    Update rotate_list_by_num.py
    Updated expected output

--- MyTraining_latest/MyTraining/001.Interview_programs/list(array)_questions/rotate_list_by_num.py
+++ MyTraining_latest/MyTraining/001.Interview_programs/list(array)_questions/rotate_list_by_num.py
@@ -1,4 +1,4 @@
-#reverse a list based on nof repeating characters suppose [1,2,3,4,5], repeat 2 numbers then output should be [2,1,4,3,5]
+#reverse a list based on nof repeating characters suppose [1,2,3,4,5], repeat 2 numbers then output should be [3, 4, 5, 1, 2]
 n=2
 list1 = [1,2,3,4,5]
 print list1[n:]+list1[:n]
